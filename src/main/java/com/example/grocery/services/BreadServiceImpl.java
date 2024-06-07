0f28958a54813f3bd3ecc5c2d23bbe5782b6e198
igd93 <102153262+igd93@users.noreply.github.com>
2024-06-07 19:47:16 +0200

    added inventory insertion at service level for vegetables and breads;

--- src/main/java/com/example/grocery/services/BreadServiceImpl.java
+++ src/main/java/com/example/grocery/services/BreadServiceImpl.java
@@ -3,18 +3,23 @@ package com.example.grocery.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.grocery.entities.Bread;
+import com.example.grocery.entities.Inventory;
 import com.example.grocery.repositories.BreadRepository;
 
 @Service
 public class BreadServiceImpl implements BreadService {
 
     private final BreadRepository breadRepository;
+    private final InventoryService inventoryService;
 
-    public BreadServiceImpl(BreadRepository breadRepository) {
+    @Autowired
+    public BreadServiceImpl(BreadRepository breadRepository, InventoryService inventoryService) {
         this.breadRepository = breadRepository;
+        this.inventoryService = inventoryService;
     }
 
     @Override
@@ -29,6 +34,10 @@ public class BreadServiceImpl implements BreadService {
 
     @Override
     public Bread save(Bread bread) {
+        Inventory inventory = new Inventory();
+        inventory.setProductType("Bread");
+        inventory = inventoryService.save(inventory);
+        bread.setInventory(inventory);
         return breadRepository.save(bread);
     }
 
--- src/main/java/com/example/grocery/services/VegetableServiceImpl.java
+++ src/main/java/com/example/grocery/services/VegetableServiceImpl.java
@@ -3,18 +3,24 @@ package com.example.grocery.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.grocery.entities.Inventory;
 import com.example.grocery.entities.Vegetable;
+import com.example.grocery.repositories.InventoryRepository;
 import com.example.grocery.repositories.VegetableRepository;
 
 @Service
 public class VegetableServiceImpl implements VegetableService {
 
     private final VegetableRepository vegetableRepository;
+    private final InventoryRepository inventoryRepository;
 
-    public VegetableServiceImpl(VegetableRepository vegetableRepository) {
+    @Autowired
+    public VegetableServiceImpl(VegetableRepository vegetableRepository, InventoryRepository inventoryRepository) {
         this.vegetableRepository = vegetableRepository;
+        this.inventoryRepository = inventoryRepository;
     }
 
     @Override
@@ -29,6 +35,10 @@ public class VegetableServiceImpl implements VegetableService {
 
     @Override
     public Vegetable save(Vegetable vegetable) {
+        Inventory inventory = new Inventory();
+        inventory.setProductType("Vegetable");
+        inventory = inventoryRepository.save(inventory);
+        vegetable.setInventory(inventory);
         return vegetableRepository.save(vegetable);
     }
 
