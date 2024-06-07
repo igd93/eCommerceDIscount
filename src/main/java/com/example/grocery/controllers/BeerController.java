6a2401bd3f3222b8a815991433adc6caabc1ea86
igd93 <102153262+igd93@users.noreply.github.com>
2024-06-08 01:05:00 +0200

    succesfully added openapi and swagger

--- src/main/java/com/example/grocery/controllers/BeerController.java
+++ src/main/java/com/example/grocery/controllers/BeerController.java
@@ -6,64 +6,84 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.grocery.entities.Beer;
 import com.example.grocery.services.BeerService;
 
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/beers")
+@Tag(name = "Beer Controller", description = "API for managing beers")
 public class BeerController {
 
-    
     private final BeerService beerService;
 
     @Autowired
-    public BeerController(BeerService beerService){
+    public BeerController(BeerService beerService) {
         this.beerService = beerService;
     }
 
+    @Operation(summary = "Get all beers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found all beers", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Beer.class)) }),
+            @ApiResponse(responseCode = "404", description = "Beers not found", content = @Content)
+    })
     @GetMapping
     public List<Beer> getAllBeers() {
-        return beerService.findAll();        
+        return beerService.findAll();
     }
 
+    @Operation(summary = "Get beer by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the beer", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Beer.class)) }),
+            @ApiResponse(responseCode = "404", description = "Beer not found", content = @Content)
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Beer> getBeerById(@PathVariable("id") Long id) {
         Optional<Beer> beer = beerService.findById(id);
         return beer
                 .map(b -> new ResponseEntity<>(b, HttpStatus.OK))
-                .orElseGet(()-> ResponseEntity.notFound().build());
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Create a new beer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Beer created", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Beer.class)) }),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @PostMapping
     public ResponseEntity<?> createBeer(@RequestBody Beer beer) {
-        try{
-        Beer saveBeer = beerService.save(beer);
-        return new ResponseEntity<>(saveBeer, HttpStatus.CREATED);
+        try {
+            Beer saveBeer = beerService.save(beer);
+            return new ResponseEntity<>(saveBeer, HttpStatus.CREATED);
         } catch (Exception e) {
             System.err.println("Error creating beer" + e.getMessage());
-            return new ResponseEntity<>("Error creating bread", HttpStatus.INTERNAL_SERVER_ERROR);            
+            return new ResponseEntity<>("Error creating beer", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        
     }
 
+    @Operation(summary = "Update a beer by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Beer updated", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Beer.class)) }),
+            @ApiResponse(responseCode = "404", description = "Beer not found", content = @Content)
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Beer> updateBeer(@PathVariable("id") Long id, @RequestBody Beer beer) {
         Optional<Beer> beerOptional = beerService.findById(id);
         if (beerOptional.isPresent()) {
-            Beer existingBeer = beerOptional.get();            
-            existingBeer.setProductName(beer.getProductName());           
+            Beer existingBeer = beerOptional.get();
+            existingBeer.setProductName(beer.getProductName());
             existingBeer.setPricePerUnit(beer.getPricePerUnit());
             Beer updateBeer = beerService.save(existingBeer);
             return ResponseEntity.ok(updateBeer);
@@ -71,35 +91,42 @@ public class BeerController {
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Partially update a beer by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Beer partially updated", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Beer.class)) }),
+            @ApiResponse(responseCode = "404", description = "Beer not found", content = @Content)
+    })
     @PatchMapping("/{id}")
     public ResponseEntity<Beer> partialUpdateBeer(@PathVariable("id") Long id, @RequestBody Beer beerUpdates) {
         Optional<Beer> beerOptional = beerService.findById(id);
         if (beerOptional.isPresent()) {
             Beer existingBeer = beerOptional.get();
-
             if (beerUpdates.getProductName() != null) {
                 existingBeer.setProductName(beerUpdates.getProductName());
             }
-
             if (beerUpdates.getPricePerUnit() != null) {
                 existingBeer.setPricePerUnit(beerUpdates.getPricePerUnit());
             }
-
             Beer updateBeer = beerService.save(existingBeer);
             return ResponseEntity.ok(updateBeer);
         }
-
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Delete a beer by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Beer deleted", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Beer not found", content = @Content)
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBeer(@PathVariable("id") Long id) {
         Optional<Beer> beerOptional = beerService.findById(id);
         if (beerOptional.isPresent()) {
             beerService.deleteById(id);
-            return ResponseEntity.noContent().build();            
+            return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
     }
-    
+
 }
